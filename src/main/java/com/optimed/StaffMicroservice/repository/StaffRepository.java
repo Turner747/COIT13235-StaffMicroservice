80df17434ed476e97863a73d7a3323149324a8f0
kunshrestha <kunshrestha@hotmail.com>
2023-09-28 22:43:58 +1000

    Repositories

--- src/main/java/com/optimed/StaffMicroservice/repository/StaffRepository.java
+++ src/main/java/com/optimed/StaffMicroservice/repository/StaffRepository.java
@@ -2,12 +2,16 @@ package com.optimed.StaffMicroservice.repository;
 
 import com.optimed.StaffMicroservice.model.Staff;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface StaffRepository extends JpaRepository<Staff,Long> {
-    public List<Staff> findByFirstNameContaining(String firstName);
-    public List<Staff> findByLastNameContaining(String lastName);
+public interface StaffRepository extends JpaRepository<Staff, Long> {
+//    public List<Staff> findByFirstNameContaining(String firstName);
+//    public List<Staff> findByLastNameContaining(String lastName);
+
+//    @Query("SELECT s FROM Staff s WHERE s.role.name = :query")
+//    public List<Staff> getAllStaffByRole(String query);
 }
