e9e714b712bb50d7930d55fd15323a99253db220
kunshrestha <kunshrestha@hotmail.com>
2023-09-30 09:17:47 +1000

    ObjectMapper created as customized ModelMapper to map object or list of object

---
+++ src/main/java/com/optimed/StaffMicroservice/mapper/ObjectMapper.java
@@ -0,0 +1,26 @@
+package com.optimed.StaffMicroservice.mapper;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ObjectMapper {
+    private static ModelMapper model_mapper;
+    static {
+        model_mapper = new ModelMapper();
+        model_mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+    }
+    private ObjectMapper() {
+    }
+    public static <D, T> D map(final T entity, Class<D> outClass) {
+        return model_mapper.map(entity, outClass);
+    }
+    public static <D, T> List<D> mapAll(final Collection<T> entityList, Class<D> outCLass) {
+        return entityList.stream()
+                .map(entity -> map(entity, outCLass))
+                .collect(Collectors.toList());
+    }
+}
