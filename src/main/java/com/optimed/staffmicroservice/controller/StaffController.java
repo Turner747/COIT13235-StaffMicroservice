300c1c36ad56e895cfa01d6cf4783557db04d782
kunshrestha <kunshrestha@hotmail.com>
2023-10-09 12:30:10 +1100

    Added endpoint to get staff by email. Changed saveStaff to receive StaffResponse instead of Staff

--- src/main/java/com/optimed/staffmicroservice/controller/StaffController.java
+++ src/main/java/com/optimed/staffmicroservice/controller/StaffController.java
@@ -1,13 +1,16 @@
 package com.optimed.staffmicroservice.controller;
 
 import com.optimed.staffmicroservice.mapper.ObjectMapper;
+import com.optimed.staffmicroservice.model.Role;
 import com.optimed.staffmicroservice.model.Staff;
 import com.optimed.staffmicroservice.repository.StaffRepository;
 import com.optimed.staffmicroservice.response.StaffResponse;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.Collection;
 import java.util.List;
@@ -18,9 +21,10 @@ import java.util.Optional;
 @RequestMapping("/restapi/staffs")
 public class StaffController {
     private StaffRepository staffRepo;
-    @PostMapping
-    public ResponseEntity<StaffResponse> saveProduct(@RequestBody Staff staff) {
-        Staff newStaff = staffRepo.save(staff);
+    @PostMapping(consumes = {"application/json"})
+    public ResponseEntity<StaffResponse> saveStaff(@RequestBody StaffResponse staffResponse) {
+        Staff newStaff = staffRepo.save(ObjectMapper.map(staffResponse, Staff.class));
+        System.out.println(newStaff);
         return ResponseEntity.status(HttpStatus.CREATED).body(ObjectMapper.map(newStaff, StaffResponse.class));
     }
     @GetMapping
@@ -31,22 +35,18 @@ public class StaffController {
         List<StaffResponse> staffResponses = ObjectMapper.mapAll(staffs, StaffResponse.class);
         return ResponseEntity.status(HttpStatus.OK).body(staffResponses);
     }
-    @PutMapping("/id/{id}")
-    public ResponseEntity<StaffResponse> updateStaff(@PathVariable Long id, @RequestBody Staff updatedStaff) {
-        Optional<Staff> optional = staffRepo.findById(id);
-        if (optional.isEmpty()) {
-            return ResponseEntity.notFound().build();
-        }
-        Staff existingStaff = optional.get();
-        existingStaff = ObjectMapper.map(updatedStaff, Staff.class);
-
-        StaffResponse staffResponse = ObjectMapper.map(staffRepo.save(existingStaff), StaffResponse.class);
-
-        return ResponseEntity.status(HttpStatus.OK).body(staffResponse);
-    }
     @GetMapping("/id/{id}")
     public ResponseEntity<StaffResponse> getStaffById(@PathVariable("id") long id) {
         Optional<Staff> optional = staffRepo.findById(id);
+        if(optional.isPresent()) {
+            StaffResponse staffResponse = ObjectMapper.map(optional, StaffResponse.class);
+            return ResponseEntity.status(HttpStatus.OK).body(staffResponse);
+        }
+        return ResponseEntity.notFound().build();
+    }
+    @GetMapping("email/{email}")
+    public ResponseEntity<StaffResponse> getStaffByEmail(@PathVariable("email") String email) {
+        Optional<Staff> optional = staffRepo.findByEmail(email);
         if(optional.isPresent()) {
             StaffResponse staffResponse = ObjectMapper.map(optional, StaffResponse.class);
             return ResponseEntity.status(HttpStatus.OK).body(staffResponse);
