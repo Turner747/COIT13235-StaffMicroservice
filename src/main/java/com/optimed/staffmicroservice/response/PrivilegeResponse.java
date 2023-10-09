7a8ad8a0fd94c54e13618d6d6ccb0908876cc193
kunshrestha <kunshrestha@hotmail.com>
2023-10-09 12:27:04 +1100

    Added @JsonIgnore

--- src/main/java/com/optimed/staffmicroservice/response/PrivilegeResponse.java
+++ src/main/java/com/optimed/staffmicroservice/response/PrivilegeResponse.java
@@ -1,5 +1,6 @@
 package com.optimed.staffmicroservice.response;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.optimed.staffmicroservice.model.Role;
 import lombok.Data;
 
@@ -8,5 +9,6 @@ import java.util.Collection;
 public class PrivilegeResponse {
     private Long id;
     private String name;
+    @JsonIgnore
     private Collection<Role> roles;
 }
