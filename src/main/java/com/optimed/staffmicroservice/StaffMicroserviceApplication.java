2f5e1b6fd848f65db3cfbe67dfc82e47a7dd30aa
kunshrestha <kunshrestha@hotmail.com>
2023-10-09 12:30:28 +1100

    Housekeeping

--- src/main/java/com/optimed/staffmicroservice/StaffMicroserviceApplication.java
+++ src/main/java/com/optimed/staffmicroservice/StaffMicroserviceApplication.java
@@ -1,24 +1,19 @@
-package com.optimed.StaffMicroservice;
+package com.optimed.staffmicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableAutoConfiguration
-@EnableJpaRepositories(basePackages = "com.optimed.StaffMicroservice.repository")
-@EntityScan(basePackages = "com.optimed.StaffMicroservice.model")
-//@ComponentScan(basePackages = "com.optimed.StaffMicroservice.service")
-//@ConfigurationPropertiesScan(basePackages = "com.optimed.StaffMicroservice.config")
+@EnableDiscoveryClient
+@EnableJpaRepositories(basePackages = "com.optimed.staffmicroservice.repository")
+@EntityScan(basePackages = "com.optimed.staffmicroservice.model")
 public class StaffMicroserviceApplication {
-
 	public static void main(String[] args) {
-
 		SpringApplication.run(StaffMicroserviceApplication.class, args);
 	}
-
 }
