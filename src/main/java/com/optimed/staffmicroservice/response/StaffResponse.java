378aae98db0fe1a0ae33e8e9ab81d5be5c8754c6
kunshrestha <kunshrestha@hotmail.com>
2023-10-09 12:32:16 +1100

    DOB format fix. Added @JsonProperty to accurately map role when nested inside Staff

--- src/main/java/com/optimed/staffmicroservice/response/StaffResponse.java
+++ src/main/java/com/optimed/staffmicroservice/response/StaffResponse.java
@@ -1,12 +1,10 @@
 package com.optimed.staffmicroservice.response;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.optimed.staffmicroservice.model.Role;
-import com.optimed.staffmicroservice.model.Shift;
-import jakarta.persistence.*;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
-import java.util.Collection;
 import java.util.Date;
 
 @Data
@@ -14,19 +12,17 @@ public class StaffResponse {
     private long id;
     private String firstName;
     private String lastName;
-    @Temporal(TemporalType.DATE)
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date dob;
     private String street;
     private String suburb;
     private String state;
-    private int postcode;
+    private String postcode;
     private String phone;
     private String email;
     private String password;
     private String providerNumber;
     private String prescriberNumber;
-    @JsonIgnore
-    private Collection<Shift> shifts;
-    //    private Collection<VisitNote> visit_notes;
-    private Collection<Role> roles;
+    @JsonProperty("role")
+    private Role role;
 }
