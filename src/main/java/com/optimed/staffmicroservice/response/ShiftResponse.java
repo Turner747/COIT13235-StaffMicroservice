9dd7052427876e4dfdca959d480a656a6fe210db
kunshrestha <kunshrestha@hotmail.com>
2023-10-10 13:32:08 +1100

    @JsonProperty and datetime format fix

--- src/main/java/com/optimed/staffmicroservice/response/ShiftResponse.java
+++ src/main/java/com/optimed/staffmicroservice/response/ShiftResponse.java
@@ -1,16 +1,21 @@
 package com.optimed.staffmicroservice.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.optimed.staffmicroservice.model.Staff;
 import jakarta.persistence.*;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
 @Data
 public class ShiftResponse {
     private long id;
+    @JsonProperty("staff")
     private Staff staff;
     @Temporal(TemporalType.TIMESTAMP)
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm")
     private Date startTime;
     @Temporal(TemporalType.TIMESTAMP)
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm")
     private Date finishTime;
 }
