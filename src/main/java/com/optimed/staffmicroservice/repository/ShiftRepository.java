4716e9362819951b6785ad39b41eed21529a8a38
kunshrestha <kunshrestha@hotmail.com>
2023-10-12 18:09:08 +1100

    Get shift by staff id query

--- src/main/java/com/optimed/staffmicroservice/repository/ShiftRepository.java
+++ src/main/java/com/optimed/staffmicroservice/repository/ShiftRepository.java
@@ -2,8 +2,13 @@ package com.optimed.staffmicroservice.repository;
 
 import com.optimed.staffmicroservice.model.Shift;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ShiftRepository extends JpaRepository<Shift, Long> {
+    @Query("SELECT s FROM Shift s WHERE s.staff.id = :id")
+    public List<Shift> findShiftByStaffId(long id);
 }
