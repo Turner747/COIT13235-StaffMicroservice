23034c6a649f2d223601695438222d621d7754ed
kunshrestha <kunshrestha@hotmail.com>
2023-10-12 18:10:40 +1100

    getAllDoctors only end point query

--- src/main/java/com/optimed/staffmicroservice/repository/StaffRepository.java
+++ src/main/java/com/optimed/staffmicroservice/repository/StaffRepository.java
@@ -2,16 +2,15 @@ package com.optimed.staffmicroservice.repository;
 
 import com.optimed.staffmicroservice.model.Staff;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface StaffRepository extends JpaRepository<Staff, Long> {
     public Optional<Staff> findByEmail(String email);
-//    public List<Staff> findByFirstNameContaining(String firstName);
-//    public List<Staff> findByLastNameContaining(String lastName);
-
-//    @Query("SELECT s FROM Staff s WHERE s.role.name = :query")
-//    public List<Staff> getAllStaffByRole(String query);
+    @Query("SELECT s FROM Staff s WHERE s.prescriberNumber IS NOT NULL")
+    public List<Staff> findAllDoctors();
 }
